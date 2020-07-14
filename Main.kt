b5f26f62e071bcaa7c49f24bc871322906274e00
luisfelipeluis49 <luisfelipeluis49@gmail.com>
2020-07-13 22:26:43 -0300

    headache, I'll finish it tomorow

--- Main.kt
+++ Main.kt
@@ -1,56 +1,73 @@
 package search
 
+import java.awt.image.BufferStrategy
 import java.io.File
 import java.util.*
 
 // MutableMap and MutableList
 
 fun main(args: Array<String>) {
-    val scanner = Scanner(System.`in`)
-    val people = File(args[1]).readLines()
-    val people1 = mutableMapOf<Int, List<String>>()
-    for (line in 1..File(args[1]).readLines().size) {
-        people1[line] = people[line - 1].split(" ")
-    }
 
-    do {
-        val keep = menu(scanner, people, people1)
-    } while (keep)
+
 
 }
 
-fun menu(scanner: Scanner, people: List<String>, people1: MutableMap<Int, List<String>>): Boolean {
-    println("=== Menu ===")
-    println("1. Find a person")
-    println("2. Print all people")
-    println("0. Exit")
-    when (scanner.next()) {
-        "1" -> find(scanner, people, people1)
-        "2" -> people.forEach(::println)
-        "0" -> { println("Bye!"); return false }
-        else -> println("Incorrect option! Try again.")
-    }
-    return true
-}
+class SearchEngine {
+    private val scanner = Scanner(System.`in`)
+    private val people1 = mutableMapOf<Int, List<String>>()
+    private lateinit var people: List<String>
 
-fun find(scanner: Scanner, people: List<String>, people1: MutableMap<Int, List<String>>) {
-    println("Enter a name or email to search all suitable people.")
-    val search = scanner.next().toLowerCase()
-    val founded = mutableListOf<Int>()
-    repeat(people1.size) {
-        val line = people1[it]
-        line?.forEach {
-            w ->
-            if (w.toLowerCase() == search) {
-                founded.add(founded.size, it)
-            }
+    fun init(args: Array<String>) {
+        people = File(args[1]).readLines()
+        for (line in 1..File(args[1]).readLines().size) {
+            people1[line] = people[line - 1].split(" ")
         }
     }
 
-    if (founded.isNotEmpty()) {
-        println("Found people:")
-        founded.forEach { println(people[it - 1])}
-    } else {
-        println("No matching people found.")
+    fun keep() {
+        do {
+            val keep = menu()
+        } while (keep)
+    }
+
+    private fun menu(): Boolean {
+        println("=== Menu ===")
+        println("1. Find a person")
+        println("2. Print all people")
+        println("0. Exit")
+        when (scanner.next()) {
+            "1" -> find(strategySearch())
+            "2" -> people.forEach(::println)
+            "0" -> { println("Bye!"); return false }
+            else -> println("Incorrect option! Try again.")
+        }
+        return true
+    }
+
+    private fun strategySearch(): String {
+        println("Select a matching strategy: ALL, ANY, NONE")
+        return scanner.nextLine()
+    }
+
+    private fun find(strategy: String) {
+        println("Enter a name or email to search all suitable people.")
+        val search = scanner.next().toLowerCase()
+        val founded = mutableListOf<Int>()
+        repeat(people1.size) {
+            val line = people1[it]
+            line?.forEach {
+                w ->
+                if (w.toLowerCase() == search) {
+                    founded.add(founded.size, it)
+                }
+            }
+        }
+
+        if (founded.isNotEmpty()) {
+            println("Found people:")
+            founded.forEach { println(people[it - 1])}
+        } else {
+            println("No matching people found.")
+        }
     }
 }
